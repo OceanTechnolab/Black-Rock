b12e3406abd131c697fc8afa5baac66f79e3fff0
Mit Joshi <mitjoshi0207@gmail.com>
2025-02-16 12:15:49 +0530

    Image loading issue fixed.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
+// filepath: /D:/Code/Black-Rock/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  metadataBase: new URL(process.env.NEXT_PUBLIC_WEB_URL || 'https://ocean.blackrockporcelain.com/'),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_WEB_URL || 'http://localhost:3000/'),
   // Ensure trailingSlash is set to true for static export
   trailingSlash: true,
-  assetPrefix: process.env.NEXT_PUBLIC_WEB_URL || 'https://ocean.blackrockporcelain.com/',
+  assetPrefix: process.env.NEXT_PUBLIC_WEB_URL || 'http://localhost:3000/',
   basePath: '',
+  images: {
+      unoptimized: true,
+  },
   // ...existing code...
 };
 
 export default nextConfig;
-
-// Add the following line to your .env file
-// NEXT_PUBLIC_WEB_URL=https://blackrockporcelain.com
